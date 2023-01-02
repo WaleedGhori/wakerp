4d2287fe99a13458ef1b0d25fb265157784576aa
WaleedGhori <waleedghori4@gmail.com>
2023-01-02 19:38:18 +0500

    api integrated

--- src/Pages/CreateInvoice.js
+++ src/Pages/CreateInvoice.js
@@ -1,76 +1,140 @@
-import React,{useContext, useState} from 'react'
+import React, { useContext, useState } from 'react'
 import ProductContext from '../context/product/ProductContext'
+import { useParams } from "react-router-dom"
 
 const CreateInvoice = () => {
   const context = useContext(ProductContext)
-  const {createCustomerInvocie} = context;
+  const { createCustomerInvocie } = context;
+  const { id } = useParams()
 
+  // const [invoice, setInvoice] = useState({cus_name:"", p_Id:"", p_name:"", pro_quantity:""})
+  const [cus_name, setCusName] = useState("")
+  const [p_Id, setProductId] = useState("")
+  const [p_name, setProductName] = useState("")
+  const [pro_quantity, setProductQuantity] = useState("")
 
-  const [invoice  , setInvoice] = useState({c_Id:"",cus_name:" ",p_Id:" ",p_name:" ",pro_quantity:" ",t_ammount:" ",a_recived:" ",bal_ammount:" ",discount:" "})
-  
-  const onChange = (e)=>{
-    setInvoice({...invoice,[e.target.value]:e.target.name})
+  const handleChange = async(e) => {
+    if (e.target.name=="cus_name") {
+      setCusName(e.target.value)
+    }
+    else if (e.target.name=="p_Id") {
+      setProductId(e.target.value)
+    }
+    else if (e.target.name=="p_name") {
+      setProductName(e.target.value)
+    }
+    else if (e.target.name=="pro_quantity") {
+      setProductQuantity(e.target.value)
+    }
+    // setInvoice({ ...invoice,[e.target.value]:e.target.name })
+    console.log(e);
   }
 
-  const handleClick= ()=>{
+  const handleClick = async(e) => {
+    e.preventDefault();
 
+    let id = p_Id;
+    let a = await fetch(`http://localhost:5000/api/product/getproduct/${id}`)
+    let res = await a.json()
+    console.log(res);
+    console.log("This is a id",id);
   }
   return (
-      <div className='container'>
-        <h2 className='text-4xl mt-2 mb-4'>Create a Invoice</h2>
-      <form>
-        <div className="mb-3">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.cus_name} name="p_name" onChange={onChange}/>
-        </div>
-
-      <div className='flex'>
-        <div className='w-1/2 flex '>
-        <div className="mb-3 w-1/3 mx-auto">
-         <label for="product name" className="form-label">1</label>
-          <input type="text" className="form-control" id="productname" value={invoice.p_Id} name="p_name" onChange={onChange}/>
-        </div>
-        <div className="mb-3 w-2/3 mx-auto">
-         <label for="product name" className="form-label">2</label>
-          <input type="text" className="form-control" id="productname" value={invoice.p_Id} name="p_name" onChange={onChange}/>
-        </div>
-        </div>
-        <div className='w-1/2 flex'>
-        <div className="mb-3 w-1/2 mx-auto">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.p_Id} name="p_name" onChange={onChange}/>
-        </div>
-        <div className="mb-3 w-1/2 mx-auto">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control btn bg" id="productname" value={invoice.p_Id} name="p_name" onChange={onChange}/>
-        </div>
-        </div>
+    <div className='container'>
+      <h2 className='text-4xl mt-2 mb-4'>Create a Invoice</h2>
+      <div>
+      <div className="mb-3 flex flex-col">
+       <label for="product name" className="form-label">Customer Name</label>
+        <input type="text" className="form-control border-2 border-slate-200 rounded mt-2" id="productname" value={cus_name} name="cus_name" onChange={handleChange}/>
       </div>
-
-
-
-
-        <div className="mb-3">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.p_Id} name="p_name" onChange={onChange}/>
+      </div>
+      <form>
+        <div className='flex'>
+          <div className='flex w-1/2'>
+            <div className="mb-3 flex flex-col w-1/3 m-auto">
+              <label for="product name" className="form-label">Product ID</label>
+              <input type="number" className="form-control border-2 border-slate-200 rounded mt-2" id="productname" value={p_Id} name="p_Id" onChange={handleChange} />
+            </div>
+            <div className="mb-3 flex flex-col w-3/5 m-auto">
+              <label for="productcategory" className="form-label">Product Name</label>
+              <input type="text" className="form-control border-2 border-slate-200 rounded mt-2" id="productcategory" value={p_name} name="p_name" onChange={handleChange} />
+            </div>
+          </div>
+          <div className='flex w-1/2'>
+            <div className="mb-3 flex flex-col w-3/5 m-auto">
+              <label for="product name" className="form-label">Product Qunatity</label>
+              <input type="number" className="form-control border-2 border-slate-200 rounded mt-2" id="productname" value={pro_quantity} name="pro_quantity" onChange={handleChange} />
+            </div>
+            <div className="mb-3 flex flex-col w-1/3 m-auto">
+            <button type="submit" className="btn addcolor px-2 py-1 text-white rounded mt-2" onClick={handleClick}>Add Product</button>
+            </div>
+          </div>
         </div>
-        <div className="mb-3">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.p_name} name="p_name" onChange={onChange}/>
-        </div>
-        <div className="mb-3">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.pro_quantity} name="p_name" onChange={onChange}/>
-        </div>
-        <div className="mb-3">
-         <label for="product name" className="form-label">Customer Name</label>
-          <input type="text" className="form-control" id="productname" value={invoice.t_ammount} name="p_name" onChange={onChange}/>
-        </div>
-
-          <button type="submit" className="btn addcolor text-white"onClick={handleClick}>Add Product</button>
-    </form>
+      </form>
+<div className='flex border-2 overflow-x-scroll mt-4'>
+<div className='table w-full p-2'>
+        <table className='w-full border' >
+          <thead>
+            <tr className="bg-gray-50 border-b">
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product ID</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Name</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Company</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Price</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Quantity</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Discount%</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Product Ex Discount%</th>
+              <th className='p-2 border-r cursor-pointer text-sm font-thin text-gray-500'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+                <td className="p-2 border-r">
+                        <input type="text" class="border p-1"/>
+                </td>
+            </tr>
+            <tr class="bg-gray-100 text-center border-b text-sm text-gray-600">
+                    <td class="p-2 border-r">2</td>
+                    <td class="p-2 border-r">Adam Smith</td>
+                    <td class="p-2 border-r">adam@gmail.com</td>
+                    <td class="p-2 border-r">Sydney, Australia</td>
+                    <td class="p-2 border-r">Sydney, Australia</td>
+                    <td class="p-2 border-r">Sydney, Australia</td>
+                    <td class="p-2 border-r">Sydney, Australia</td>
+                    <td>
+                        <a href="#" class="bg-blue-500 p-2 text-white hover:shadow-lg text-xs font-thin">Edit</a>
+                        <a href="#" class="bg-red-500 p-2 text-white hover:shadow-lg text-xs font-thin">Remove</a>
+                    </td>
+                </tr>
+          </tbody>
+        </table>
+</div>
+</div>
+     
     </div>
   )
 }
 
 export default CreateInvoice
+
+
